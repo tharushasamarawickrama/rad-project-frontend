5034e841af9a18f7ecd3fab779573b08cd3264a4
kalpasuraweera <kalpamadhushan18@gmail.com>
2024-08-05 23:16:46 +0530

    create navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,35 @@
-import React from "react";
-import { AppBar, Toolbar, Typography } from "@mui/material";
+import React, { useState } from "react";
+import {
+  AppBar,
+  Box,
+  Drawer,
+  IconButton,
+  MenuItem,
+  Toolbar,
+  Typography,
+  useMediaQuery,
+} from "@mui/material";
 import { ReactComponent as Logo } from "../assets/logo.svg";
+import { useTheme } from "@emotion/react";
+import { useNavigate } from "react-router-dom";
+import MenuIcon from "@mui/icons-material/Menu";
 
 const NavBar = () => {
+  const theme = useTheme();
+  const navigation = useNavigate();
+  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  const menuItems = [
+    { text: "Home", path: "/" },
+    { text: "About", path: "/about" },
+    { text: "Contact", path: "/contact" },
+  ];
+
+  const handleNavigation = (path) => {
+    navigation(path);
+    setDrawerOpen(false);
+  };
   return (
     <AppBar
       position="static"
@@ -13,6 +40,46 @@ const NavBar = () => {
     >
       <Toolbar>
         <Logo height={70} />
+        {isMobile ? (
+          <>
+            <IconButton
+              edge="end"
+              color="inherit"
+              onClick={() => setDrawerOpen(!drawerOpen)}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Drawer
+              anchor="right"
+              open={drawerOpen}
+              onClose={() => setDrawerOpen(false)}
+            >
+              <Box sx={{ width: 250, paddingTop: 2 }}>
+                {menuItems.map((item) => (
+                  <MenuItem
+                    key={item.text}
+                    onClick={() => handleNavigation(item.path)}
+                  >
+                    <Typography textAlign="center">{item.text}</Typography>
+                  </MenuItem>
+                ))}
+              </Box>
+            </Drawer>
+          </>
+        ) : (
+          <Box
+            sx={{ display: "flex", justifyContent: "flex-end", flexGrow: 1 }}
+          >
+            {menuItems.map((item) => (
+              <MenuItem
+                key={item.text}
+                onClick={() => handleNavigation(item.path)}
+              >
+                <Typography>{item.text}</Typography>
+              </MenuItem>
+            ))}
+          </Box>
+        )}
       </Toolbar>
     </AppBar>
   );
