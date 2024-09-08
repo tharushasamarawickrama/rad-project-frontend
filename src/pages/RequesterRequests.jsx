b69a69e8e6c7076d43797b2154fee30dbfc817be
Piyumi <piyuminavodya2003@gmail.com>
2024-09-08 23:01:59 +0530

    Merge remote-tracking branch 'origin/main' into K-10

--- src/pages/RequesterRequests.jsx
+++ src/pages/RequesterRequests.jsx
@@ -156,7 +156,7 @@ export default function RequesterRequests() {
               }}
             >
               <Typography variant="h4" component="div" color="primary">
-                Requests
+                All Requests
               </Typography>
               <Button
                 variant="contained"
